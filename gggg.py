c5067ee088a577b532b2f2c4d953279a2989dbdd
dkdhWkwmdsk <149846616+dkdhWkwmdsk@users.noreply.github.com>
2023-11-11 10:45:25 +0900

    Update gggg.py

--- gggg.py
+++ gggg.py
@@ -1,33 +1,41 @@
 import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def main():
-    st.title("간단한 계산기")
+    st.title("Iris 데이터셋 시각화")
 
-    # 사용자로부터 숫자 입력 받기
-    num1 = st.number_input("첫 번째 숫자를 입력하세요", value=0.0)
-    num2 = st.number_input("두 번째 숫자를 입력하세요", value=0.0)
+    # Iris 데이터셋 로드
+    iris_df = load_iris_dataset()
 
-    # 연산 선택하기
-    operation = st.selectbox("연산을 선택하세요", ["덧셈", "뺄셈", "곱셈", "나눗셈"])
+    # 사용자로부터 꽃 종류 선택
+    selected_species = st.selectbox("꽃 종류 선택", iris_df['species'].unique())
 
-    # 계산 수행
-    result = calculate(num1, num2, operation)
+    # 선택한 꽃 종류의 데이터 필터링
+    selected_data = iris_df[iris_df['species'] == selected_species]
 
-    # 결과 출력
-    st.write(f"결과: {result}")
+    # 선택한 꽃 종류의 특성 시각화
+    visualize_data(selected_data)
 
-def calculate(num1, num2, operation):
-    if operation == "덧셈":
-        return num1 + num2
-    elif operation == "뺄셈":
-        return num1 - num2
-    elif operation == "곱셈":
-        return num1 * num2
-    elif operation == "나눗셈":
-        if num2 != 0:
-            return num1 / num2
-        else:
-            return "두 번째 숫자는 0이 될 수 없습니다."
+def load_iris_dataset():
+    # seaborn의 Iris 데이터셋 로드
+    iris = sns.load_dataset('iris')
+    return iris
+
+def visualize_data(data):
+    # 꽃 종류에 따른 특성 시각화
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+
+    # 꽃받침 길이와 꽃받침 너비의 관계
+    sns.scatterplot(x='sepal_length', y='sepal_width', data=data, hue='species', ax=ax1)
+    ax1.set_title('Sepal Length vs Sepal Width')
+
+    # 꽃잎 길이와 꽃잎 너비의 관계
+    sns.scatterplot(x='petal_length', y='petal_width', data=data, hue='species', ax=ax2)
+    ax2.set_title('Petal Length vs Petal Width')
+
+    st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
