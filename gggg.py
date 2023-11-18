21656364755310d19abbc39207a650a30080f99b
dkdhWkwmdsk <149846616+dkdhWkwmdsk@users.noreply.github.com>
2023-11-18 09:51:17 +0900

    Update gggg.py

--- gggg.py
+++ gggg.py
@@ -4,4 +4,5 @@ import streamlit as st
 import pandas as pd
 st.title('Baeks Portfolio')
 tab1, tab2, tab3, tab4 = st.tabs(["Home", "About me","Projects","Career"])
-st.title('Titanic Dataset')
+st.subheader('This is a subheader with a divider', divider='rainbow')
+st.subheader('titanic :blue[Dataset] :sunglasses:')
