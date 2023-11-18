b603d34e75abc7457d18b09b15398e5ea091c0b3
dkdhWkwmdsk <149846616+dkdhWkwmdsk@users.noreply.github.com>
2023-11-18 10:18:27 +0900

    Update gggg.py

--- gggg.py
+++ gggg.py
@@ -6,4 +6,5 @@ st.title('Baeks Portfolio')
 tab1, tab2, tab3, tab4 = st.tabs(["Home", "About me","Projects","Career"])
 with tab1:
    st.header("Titanic Dataset")
-   st.image()
+   df=pd.read_csv("2023-11-18T01-14_export.csv")
+   st.dataframe(df)
