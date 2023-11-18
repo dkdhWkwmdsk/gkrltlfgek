01e9a2e448f8faeca3d7bc0a3ecf5917cda59dd9
dkdhWkwmdsk <149846616+dkdhWkwmdsk@users.noreply.github.com>
2023-11-18 09:47:18 +0900

    Update gggg.py

--- gggg.py
+++ gggg.py
@@ -3,5 +3,4 @@
 import streamlit as st
 import pandas as pd
 st.title('Baeks Portfolio')
-st.title('_Streamlit_ is :blue[cool] :sunglasses:')
 tab1, tab2, tab3, tab4 = st.tabs(["Home", "About me","Projects","Career"])
